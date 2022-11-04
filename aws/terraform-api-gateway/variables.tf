2a04728fe379771b84151915c0235a4d3b1b1b55
teesingh1981 <teesingh@hotmail.com>
2022-11-04 11:03:51 +0000

    added aws api gw module example

---
+++ aws/terraform-api-gateway/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "The region in which to create/manage resources"
+  default     = "eu-west-2"
+}
+
+variable "account_id" {
+  type        = string
+  description = "The account ID in which to create/manage resources"
+  default     = ""
+}
